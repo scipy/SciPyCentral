df0ca0ce3565be89c64fd41ecfa861aab7a1a479
Kevin Dunn <kgdunn@gmail.com>
2011-06-19 11:06:23 -0400

    More work on tagging and saving tags in a new revision.

---
+++ scipy_central/tagging/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from models import Tag
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'tag_type', 'description')
+    list_display_links = ('slug',)
+    list_per_page = 1000
+    ordering = ('slug',)
+
+admin.site.register(Tag, TagAdmin)
