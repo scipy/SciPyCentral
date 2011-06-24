248f150429629cece18db7977cfdd83d297685d5
Kevin Dunn <kgdunn@gmail.com>
2011-06-23 21:12:58 -0400

    Added some more illustrations.

--- scipy_central/screenshot/forms.py
+++ scipy_central/screenshot/forms.py
@@ -5,6 +5,6 @@ class ScreenshotForm(forms.Form):
     Upload a screenshot for the submission
     """
     label = 'Upload a screenshot, if you have one available'
-    help_text = 'JPEG or PNG (will be resized to 128x128'
+    help_text = 'JPEG or PNG (will be resized to 128x128)'
     screenshot = forms.ImageField(label=label, help_text=help_text,
                                   required=False)
