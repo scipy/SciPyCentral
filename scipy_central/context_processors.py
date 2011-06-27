2292df15b950bb68d2f691b81b5b79bb9926d8b2
Kevin Dunn <kgdunn@gmail.com>
2011-06-27 13:48:15 -0400

    Styling for the headers and footers mostly complete

--- scipy_central/context_processors.py
+++ scipy_central/context_processors.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from datetime import datetime
 
 def global_template_variables(request):
     return {'JQUERY_URL': settings.JQUERY_URL,
              'JQUERYUI_URL': settings.JQUERYUI_URL,
              'JQUERYUI_CSS': settings.JQUERYUI_CSS,
+             'THE_YEAR': str(datetime.now().year),
             }
