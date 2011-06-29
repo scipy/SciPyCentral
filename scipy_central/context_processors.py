10ece2b69b48f2078c8ed71aba86e4b40773a6c8
Kevin Dunn <kgdunn@gmail.com>
2011-06-29 10:08:55 -0400

    Moved horizontal scrollbar; moved styles into CSS.

--- scipy_central/context_processors.py
+++ scipy_central/context_processors.py
@@ -9,26 +9,9 @@ def global_template_variables(request):
              'THE_YEAR': str(datetime.now().year),
              # Assume the submitting user is not validated, by default
              'validated_user': False,
+
+             # Inc
              'TAGGING_JAVSCRIPT': """
-<style>
-	.ui-autocomplete-loading {
-        background: white url('/media/ui-anim_basic_16x16.gif') right center no-repeat;
-    }
-	.ui-autocomplete {
-		max-height: 150px;
-		overflow-y: auto;
-		/* prevent horizontal scrollbar */
-		overflow-x: hidden;
-		/* add padding to account for vertical scrollbar */
-		padding-right: 20px;
-	}
-	/* IE 6 doesn't support max-height
-	 * we use height instead, but this forces the menu to always be this tall
-	 */
-	* html .ui-autocomplete {
-		height: 100px;
-	}
-</style>
 <script>
 <!--JQuery example: http://jqueryui.com/demos/autocomplete/#multiple-remote -->
 $(function() {
